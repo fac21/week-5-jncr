53172f7b8dc12f055658d4dfc7a820e70a1f603b
Craig MacLeod <c92macleod@gmail.com>
2021-04-21 16:22:08 +0100

    initialise init.sql
    Co-authored-by: Jo Humphrey <31373245+jamdelion@users.noreply.github.com>
    Co-authored-by: Rosie O'Donnell <61861015+Rosie-ODonnell@users.noreply.github.com>
    Co-authored-by: ByteSized <60395899+ByteSizedIT@users.noreply.github.com>

---
+++ database/init.sql
@@ -0,0 +1,32 @@
+BEGIN;
+
+DROP TABLE IF EXISTS people, hobbies CASCADE;
+
+CREATE TABLE people (
+    github_username TEXT PRIMARY KEY,
+    name TEXT,
+    pronoun TEXT,
+    cohort INTEGER,
+    location TEXT
+);
+
+CREATE TABLE interests (
+    id SERIAL PRIMARY KEY,
+    username TEXT REFERENCES people(github_username),
+    activity TEXT
+);
+
+
+
+INSERT INTO people (github_username, name, pronoun, cohort, location) VALUES 
+('jamdelion', 'Jo', 'they', 21, 'Cambridge'),
+('tiarama', 'Craig', 'they', 21, 'London')
+;
+
+INSERT INTO interests (username, activity) VALUES 
+('jamdelion', 'roller-blading'),
+('jamdelion', 'drumming'),
+('tiarama', 'drag')
+;
+
+COMMIT;
