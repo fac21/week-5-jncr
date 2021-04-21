50c3e0ecb9e7e72a221733d46a0f4620f262e6bb
Jo Humphrey <31373245+jamdelion@users.noreply.github.com>
2021-04-21 17:17:01 +0100

    Set up cypress
        Co-authored-by: Craig MacLeod <c92macleod@gmail.com>
        Co-authored-by: Rosie O'Donnell <61861015+Rosie-ODonnell@users.noreply.github.com>
        Co-authored-by: ByteSized <60395899+ByteSizedIT@users.noreply.github.com>

--- database/connection.js
+++ database/connection.js
@@ -1,9 +1,13 @@
 const pg = require('pg');
-// const DB_URL = process.env.DATABASE_URL;
-const options = {connectionString: DB_URL};
-const db = new pg.Pool(options);
 const dotenv = require('dotenv');
 
-dotenv.config()
+dotenv.config();
+
+const DB_URL = process.env.DATABASE_URL;
 
 if (!DB_URL) throw new Error('DB_URL does not exist');
+
+const options = {connectionString: DB_URL};
+const db = new pg.Pool(options);
+
+module.exports = db;
---
+++ database/model.js
@@ -0,0 +1,7 @@
+const db = require("./connection.js");
+
+// function getPeople() {
+//     return db.query("SELECT * FROM people").then((result) => result.rows);
+//   }
+
+// module.exports = { getPeople };
