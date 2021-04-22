397ca3823b2ef567d7a1e3a468fa7ac5882ccb4e
Rosie O'Donnell <rosie.odonnell@talk21.com>
2021-04-22 11:58:03 +0100

    adding additional files to commit

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const newUser = require('./routes/new-user.js')
 const server = express();
 
 server.get('/', (req, res) => {
     res.send('HI <3')
 });
 
+const bodyParser = express.urlencoded();
+
+server.get("/new-user", newUser.newUser);
+server.post("/new-user", bodyParser, newUser.post);
+
+
 const PORT = process.env.PORT || 3000;
+
 
 server.listen(PORT, () => {
     console.log(`listening on http://localhost:${PORT}`)
